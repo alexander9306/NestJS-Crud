1b1f4234247f1566f51f0f4c723226db4861d592
alexander <alexander9306@gmail.com>
2020-11-10 04:30:15 -0400

    updated linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
     "airbnb-base",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   root: true,
   env: {
@@ -20,32 +20,42 @@ module.exports = {
     jest: true,
   },
   rules: {
-    "camelcase": [
+    camelcase: [
       "error",
       {
-        "properties": "never",
-        "ignoreDestructuring": true
-      }
+        properties: "never",
+        ignoreDestructuring: true,
+      },
     ],
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": "error",
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.js", "**/*.spec.js", "**/*.test.ts", "**/*.spec.ts"]}],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.test.js",
+          "**/*.spec.js",
+          "**/*.test.ts",
+          "**/*.spec.ts",
+        ],
+      },
+    ],
     "no-useless-constructor": "off",
     "import/prefer-default-export": "off",
     "@typescript-eslint/no-useless-constructor": ["error"],
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "import/extensions": [
       "error",
       "ignorePackages",
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-   ]
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
   },
 };
