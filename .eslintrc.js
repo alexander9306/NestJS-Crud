075eff5e7f17f81d0c7d7270028231c5f7b75313
alexander <alexander9306@gmail.com>
2020-11-10 04:16:10 -0400

    made updates to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
